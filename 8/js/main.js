8e5c425076bc6dafe94a4dab2664d1cb091d4e9f
Keks <keks@htmlacademy.ru>
2023-02-22 18:12:34 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,9 @@
+import {generateDescription, PHOTOS_COUNT} from './data.js';
+import {insertPhotoMiniature} from './photoMiniature.js';
+import {showFileForm} from './form.js';
+
+const descriptions = Array.from({length: PHOTOS_COUNT}, generateDescription);
+insertPhotoMiniature(descriptions);
+
+document.querySelector('#upload-file').addEventListener('change', showFileForm);
+
---
+++ 8/js/util.js
@@ -0,0 +1,34 @@
+export const isCorrectLength = (str, maxLength) => str.length <= maxLength;
+
+export function getRandomInt(from, to) {
+  if (from < 0 || to < 0) {
+    throw new RangeError('Числа в диапазоне должны быть положительными');
+  }
+
+  if (from === to) {
+    return from;
+  }
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+
+  return Math.round(Math.random() * (to - from) + from);
+}
+
+export function getCloseListers(modal, closeButton, callback){
+  const closeOnEscape = (ev) => ev.key === 'Escape' && closeModal();
+  function closeModal(){
+    if (callback){
+      callback();
+    }
+    modal.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    document.removeEventListener('keydown', closeOnEscape);
+    closeButton.removeEventListener('click', closeModal);
+  }
+  return [closeModal, closeOnEscape];
+}
+
+export function trimField(field) {
+  field.value = field.value.trimEnd();
+}
