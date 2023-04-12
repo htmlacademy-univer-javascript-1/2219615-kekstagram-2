38ba48be6b3ac5d0c0deaf66d6f38898a470ce92
Keks <keks@htmlacademy.ru>
2023-04-12 23:47:26 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,10 @@
+import {getMiniatures} from './api.js';
+import {insertPhotoMiniature} from './photoMiniature.js';
+import {showFileForm} from './form.js';
+
+//const descriptions = Array.from({length: PHOTOS_COUNT}, generateDescription);
+//insertPhotoMiniature(descriptions);
+getMiniatures()
+  .then(insertPhotoMiniature);
+document.querySelector('#upload-file').addEventListener('change', showFileForm);
+
